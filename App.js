a8880b3e0265b9be38853295d8b26c8acf59cf29
btonella <beatriz.tonella@gmail.com>
2019-05-29 21:26:06 -0300

    making first screen

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Picker} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,11 +18,18 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+    render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello</Text>
-        <Text style={styles.instructions}>Funcionou!!!!!</Text>
+        <Text style={styles.welcome}>Cidade Dorme</Text>
+        <Text style={styles.instructions}> </Text>
+        <Text style={styles.instructions}>Escolha o modo de jogo</Text>
+        
+        <Picker selectedValue="Online" color="#FFFFFF">
+          <Picker.Item label="Online" value="Online" />
+          <Picker.Item label="Offline" value="Offline" />
+        </Picker>
+
       </View>
     );
   }
@@ -33,16 +40,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#555555',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 40,
     textAlign: 'center',
-    margin: 10,
+    margin: 25,
+    color: '#FFFFFF'
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#FFFFFF',
+    marginBottom: 20,
   },
 });
